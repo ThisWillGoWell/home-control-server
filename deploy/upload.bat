68900f8bda2288586088363840a15726ab4c1be9
ThisWillGoWell <william.gowell@gmail.com>
2017-10-11 23:36:33 -0700

    moved into deploy folder added new systems to init

--- deploy/upload.bat
+++ deploy/upload.bat
@@ -3,4 +3,5 @@ IF "%1"=="-n" GOTO upload
 call cd ..
 call mvn install
 :upload
+call cd deploy
 call bash upload.sh
--- deploy/upload.sh
+++ deploy/upload.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 scp /mnt/c/Users/Willi/OneDrive/workspace/HomeControl/home-control-server/resources/init.json will@homecontrol.com:~/homeControl/resources/init.json
 scp /mnt/c/Users/Willi/OneDrive/workspace/HomeControl/home-control-server/target/home-control-server-0.1.0.jar will@homecontrol.com:~/homeControl/home-control-server-0.1.0.jar
